80dabedfd5fc9cf4f3626f2f7b687dbda1c60094
崔腾飞 <cuitengfei@cuitengfeideMacBook-Pro.local>
2024-12-20 00:04:37 -0800

    Fix setup.py by removing unnecessary dependencies and updating metadata

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 from setuptools import setup, find_packages
 
+# 读取长描述
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A Python package for allele specific gene expression"
+
 setup(
     name='DAESC-GPU',  
     version='1.0', 
     author='Tengfei Cui',      
     author_email='tfcui23@uw.edu',  
-    description='A Python package for allele specific gene expresion',
-    long_description=open('README.md').read(), 
+    description='A Python package for allele specific gene expression',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ITCUI-XJTLU/DAESC-GPU',  
     packages=find_packages(),  
     install_requires=[
-        'numpy', 
+        'numpy',
         'cupy',
-        'time',
-        'math',
         'pandas',
-        'csv', 
         'matplotlib',
         'scipy',
-        'gc',
-        'inspect',
-        'numpy', 
-        'pandas', 
-        'statsmodels' 
-        'scipy' 
-        'time'
+        'statsmodels',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
     python_requires='>=3.10', 
 )
